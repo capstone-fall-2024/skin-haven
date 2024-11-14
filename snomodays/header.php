813bee79e8909c16f0afe3136556916c84b7f6a6
Mohammed Al-salim <113482092+Mohammed-alsalim@users.noreply.github.com>
2024-11-14 13:45:39 -0700

    Add files via upload

--- snomodays/header.php
+++ snomodays/header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package SnoMoDays
+ * @package Skin_Haven
  */
 
 ?>
@@ -23,7 +23,7 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'snomodays' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'skin-haven' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -38,22 +38,26 @@
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
-			$snomodays_description = get_bloginfo( 'description', 'display' );
-			if ( $snomodays_description || is_customize_preview() ) :
+			$skin_haven_description = get_bloginfo( 'description', 'display' );
+			if ( $skin_haven_description || is_customize_preview() ) :
 				?>
-				<p class="site-description"><?php echo $snomodays_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<p class="site-description"><?php echo $skin_haven_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'snomodays' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'skin-haven' ); ?></button>
+			<ul class="menu">
+        <li><a href="<?php echo home_url( '/' ); ?>">Home</a></li>
+        <li><a href="<?php echo home_url( '/about/' ); ?>">About</a></li>
+        <li><a href="<?php echo home_url( '/about-us/' ); ?>">About Us</a></li>
+        <li><a href="<?php echo home_url( '/off-road-safety/' ); ?>">Off-Road Safety</a></li>
+        <li><a href="<?php echo home_url( '/events/' ); ?>">Events</a></li>
+        <li><a href="<?php echo home_url( '/gallery/' ); ?>">Gallery</a></li>
+        <li><a href="<?php echo home_url( '/get-in-touch/' ); ?>">Get in Touch</a></li>
+        <li><a href="<?php echo home_url( '/contact-us/' ); ?>">Contact Us</a></li>
+        <li><a href="<?php echo home_url( '/volunteer/' ); ?>">Volunteer</a></li>
+        <li><a href="<?php echo home_url( '/results/' ); ?>">Results</a></li>
+    </ul>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	
