d211252554c93f172249991121acab76c1c9d3cd
Rexine Jimenez <145060068+rexinej@users.noreply.github.com>
2024-11-29 14:42:28 -0700

    Add files via upload

--- snomodays/page-results.php
+++ snomodays/page-results.php
@@ -14,40 +14,54 @@
 
 get_header();
 ?>
-<Section>
+<Section class="banner-container">
     <h1>Results</h1>
     <p>Discover this year’s raffle and 50/50 winners, along with highlights from SnoMo Days events.
 
         Thank you to everyone for your participation and support!</p>
 </Section>
 </header><!-- #masthead -->
-<main id="primary" class="site-main">
-    <div class="container results">
+<main id="primary" class="site-main results">
+<div class="container">
+        <div class="fifty-fifty">
+            <div class="sub-container">
+                <img src="<?php bloginfo('stylesheet_directory');?>/img/snowcabin.jpg" alt="Snow Cabin" />
+                <div>
+                    <h2>50/50 Winner</h2>
+                    <p>$4808.00</p>
+                    <p>Mohamad Khalid</p>
+                 </div>
+            </div>
+        </div>
+    </div>
+    <div class="container grid-layout">
         <div>
             <img src="<?php bloginfo('stylesheet_directory');?>/img/snowcabin.jpg" alt="Snow Cabin" />
-            <h2>50/50 Winner</h2>
-            <p>$4808.00</p>
-            <p>Mohamad Khalid</p>
+            <div>
+                <h2>Snowmobile Drag Races</h2>
+                <p>Winner Name</p>
+            </div>
         </div>
         <div>
             <img src="<?php bloginfo('stylesheet_directory');?>/img/snowcabin.jpg" alt="Snow Cabin" />
-            <h2>Snowmobile Drag Races</h2>
-            <p>Winner Name</p>
+            <div>
+                <h2>Snowmobile Poker Rally</h2>
+                <p>Winner Name</p>
+            </div>
         </div>
         <div>
             <img src="<?php bloginfo('stylesheet_directory');?>/img/snowcabin.jpg" alt="Snow Cabin" />
-            <h2>Snowmobile Poker Rally</h2>
-            <p>Winner Name</p>
+            <div>
+                <h2>Over the Line Snowball Tournament</h2>
+                <p>Winner Name</p>
+            </div>
         </div>
         <div>
             <img src="<?php bloginfo('stylesheet_directory');?>/img/snowcabin.jpg" alt="Snow Cabin" />
-            <h2>Over the Line Snowball Tournament</h2>
-            <p>Winner Name</p>
-        </div>
-        <div>
-            <img src="<?php bloginfo('stylesheet_directory');?>/img/snowcabin.jpg" alt="Snow Cabin" />
-            <h2>Broom Ball Tournament</h2>
-            <p>Winner Name</p>
+            <div>
+                <h2>Broom Ball Tournament</h2>
+                <p>Winner Name</p>
+            </div>
         </div>
     </div>
 </main><!-- #main -->
